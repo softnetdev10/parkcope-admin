bcc768cf2c3df52048591ea7b9408541dca0d351
SOFTNET <softnet@softnet-Lenovo-Z40-70>
2019-06-11 14:11:56 -0500

    actualiza grafica

--- admin/index.js
+++ admin/index.js
@@ -2,12 +2,17 @@ $(document).ready(function(){
 //   alert('bienvenido administrador') ;
    
    $('#containerr').highcharts({
-       chart: {type : 'line', borderwidth : 0},
+       chart: {type : 'line', borderwidth : 5},
        title: {text: 'Finanzas Parkcope'},
-       xAxis: {title: {text:'Parkcope'}, categories: ["2018","2019", "2020", "2021"]},
+       xAxis: {title: {text:'Años'}, categories: ["2018","2019", "2020", "2021"]},
        yAxis: {title: {text: 'Ganancias'}, categories: ['50','100', '150', '200','250','300','350','400','500']},
        series : [
-           {name: 'Parkcope', data:[1,3,5,8]}
+           {name: 'linia 1', data:[0,500,300,500]}
+//           {name: 'Parkcope', data:[50,350,100,400]},
+//           {name: 'Parkcope', data:[50,150,100,400]},
+//           {name: 'Parkcope', data:[50,200,100,400]},
+//           {name: 'Parkcope1', data:[50,200,450,400]},
+//           {name: 'Parkcope', data:[50,150,100,200]}
            
        ]      
    });   
